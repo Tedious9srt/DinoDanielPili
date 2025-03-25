28a1af3b0984db107f47c948354da40c532e9be2
Tedious9srt <arnelkylecruz@gmail.com>
2025-03-25 18:17:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.addEventListener("load", function() {
     constructor(game) {
       this.game = game
       this.sprite = new Image();
-      this.sprite.src = "kisses.png";
+      this.sprite.src = "Assets/kisses.png";
       this.x = 700;
       this.y = 300;
       this.w = 150;
@@ -40,25 +40,27 @@ window.addEventListener("load", function() {
   }
   
   class Phone {
-    constructor(game) {
+    constructor(game, y, x) {
       this.game = game
       this.game = game;
       this.sprite = new Image();
-      this.sprite.src = "phone.png";
-      this.x = 1000;
-      this.y = 375;
+      this.sprite.src = "Assets/phone.png";
+      this.x = x;
+      this.y = y;
       this.w = 75;
       this.h = 100;
       this.cd = 0;
       this.cdm = 10;
+      this.speed = 6;
     }
     draw(ctx) {
       if (this.x != 0) {
         ctx.drawImage(this.sprite, this.x, this.y, this.w, this.h);
-        this.x -= 10
+        this.x -= this.speed;
       };
     }
     update() {
+      this.speed += 0.01;
       if (this.x <= 0) {
         this.x = 1000;
       }
@@ -69,7 +71,7 @@ window.addEventListener("load", function() {
     constructor(game) {
       this.game = game;
       this.sprite = new Image();
-      this.sprite.src = "player.png";
+      this.sprite.src = "Assets/player.png";
       this.x = 200;
       this.y = 300;
       this.w = 150;
@@ -82,9 +84,11 @@ window.addEventListener("load", function() {
       this.ftm = 9;
       this.jumping = false;
       this.score = 0;
+      this.jumpdur = 5;
     }
     update() {
       this.fts++;
+      this.jumpdur += 0.01;
       if (this.fts >= this.ftm) {
         this.fts = 0;
         this.fx = this.fx + this.fw;
@@ -94,9 +98,9 @@ window.addEventListener("load", function() {
         this.fx = 0;
       }
       if (this.jumping == true) {
-        this.y -= 15;
+        this.y -= this.jumpdur;
       } else if (this.jumping == false) {
-          this.y += 5;
+          this.y += (this.jumpdur - (this.jumpdur / 4));
       }
       if (this.y >= 300) {
         this.y = 300
@@ -107,7 +111,8 @@ window.addEventListener("load", function() {
     }
     draw(ctx) {
       ctx.font = "40px Arial";
-      ctx.fillText(this.score, 50, 50);
+      ctx.fillText("Score: " + this.score, 50, 50);
+      ctx.fillText("Made by TDSSRT(ARNEL)", 540, 32);
       ctx.drawImage(this.sprite, this.fx, this.fy, this.fw, this.fh, this.x, this.y, this.w, this.h);
     }
     jump() {
@@ -120,21 +125,27 @@ window.addEventListener("load", function() {
       this.width = width;
       this.height = height;
       this.dino = new Dino(this);
-      this.phone = new Phone(this);
-      this.kisses = new Kisses(this);
+      this.phone = new Phone(this, 375, 1000);
+      this.kisses = new Kisses(this, 300);
+      this.phone1 = new Phone(this, 100, 1500);
     }
     update() {
       this.dino.update();
       this.phone.update();
       this.kisses.update();
+      this.phone1.update();
       if ( this.dino.x < this.phone.x + this.phone.w && this.dino.x + this.dino.w > this.phone.x && this.dino.y < this.phone.y + this.phone.h && this.dino.y + this.dino.h > this.phone.y) {
           location.reload();
+      }
+      if (this.dino.x < this.phone1.x + this.phone1.w && this.dino.x + this.dino.w > this.phone1.x && this.dino.y < this.phone1.y + this.phone1.h && this.dino.y + this.dino.h > this.phone1.y) {
+        location.reload();
       }
     }
     draw(ctx) {
       this.dino.draw(ctx);
       this.kisses.draw(ctx);
       this.phone.draw(ctx);
+      this.phone1.draw(ctx);
     }
     jump() {
       this.dino.jump();
